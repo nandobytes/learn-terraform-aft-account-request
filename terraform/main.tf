cf1e6ac1af01903c127cce683fa92bdcdba25ed4
nando <fernando@nandobytes.com>
2022-10-06 11:47:57 -0400

    create sandbox-aft account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "fernando+sandbox1@nandobytes.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "fernando+sandbox1@nandobytes.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
